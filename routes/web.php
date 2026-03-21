ef353eee72f7548af34fce3aa3b80bdb6883f103
ElondoPrinceDarlins <152653687+Kolo-Michael@users.noreply.github.com>
2026-03-21 15:45:22 +0100

    The home and courses page completed

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Home page
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.home');
 });
+
+// Courses pages
+Route::get('/courses', function () {
+    return view('pages.courses');
+});
+
+// Single course page
+Route::get('/courses/{id}', function ($id) {
+    return view('pages.course');
+})->where('id', '[0-9]+');
+
+// Lesson page
+Route::get('/courses/{courseId}/lessons/{lessonId}', function ($courseId, $lessonId) {
+    return view('pages.lesson');
+})->where(['courseId' => '[0-9]+', 'lessonId' => '[0-9]+']);
